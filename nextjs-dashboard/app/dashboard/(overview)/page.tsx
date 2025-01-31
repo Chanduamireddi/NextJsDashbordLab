287e152cd1506d3f7ea2f87b77bef605d0e1879c
SaiChandu Amireddi <chanduamireddi@gmail.com>
2025-01-31 03:59:41 -0500

    Chapter 6 to 11 Code

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -30,6 +30,7 @@ export default async function Page() {
           <CardWrapper />
         </Suspense>
       </div>
+
       {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
